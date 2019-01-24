b0585fda3f0fee01108495988e44b503606322b3
Michael Allen <ma698618@ohio.edu>
2019-01-23 19:36:36 -0500

    @markamay

---
+++ git-lab-program.cc
@@ -0,0 +1,24 @@
+//***********************************************************
+//
+//  program: git-lab-program.cc
+//
+//  Name: Michael Allen
+//  Date: 1/16/19
+//  Email: ma698618@ohio.edu
+//
+//  Description: Outputs Hello
+//
+//************************************************************
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
+
